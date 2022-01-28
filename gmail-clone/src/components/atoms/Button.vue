b091915afcd74bd007b4860fb474c7d7802f9d33
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-01-28 20:15:20 +0000

    Aded basic buttons

---
+++ gmail-clone/src/components/atoms/Button.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    created: {type:Function as PropType<Function>},
+    content: {type:String as PropType<string>, required: true},
+	classes: {type:String as PropType<string>, required: true},
+  }
+})
+</script>
+
+<template>
+	<button
+	  type="button"
+		@click="this.$emit('buttonClick')"
+		class="border border-gray-200"
+		:class="classes"
+	>{{content}}</button>
+</template>
--- gmail-clone/src/views/HomeView.vue
+++ gmail-clone/src/views/HomeView.vue
@@ -1,9 +1,37 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import Button from "../components/atoms/Button.vue";
+
+export default defineComponent({
+  components: {
+    Button
+  },
+
+  setup(){
+    const handleCreate = () => {
+      console.log('Child has been created.............');
+    }
+    const selectedScreen = ref("inbox");
+    return { handleCreate, selectedScreen}
+  },
+
+})
+
+</script>
 
 
 <template>
-  <main>  
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <main>
+
+  <Button @click="handleCreate"
+  content="Inbox View"
+          :class="[selectedScreen == 'inbox' ? 'selected' : '']">
+    
+  </Button>
+  <Button @click="handleCreate"
+  content="Archived View" 
+          :class="[selectedScreen == 'archive' ? 'selected' : '']">
+    
+  </Button>
   </main>
 </template>
