ed4c10188e8817f942a100e8a535186626602772
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-01-28 20:57:28 +0000

    built mail table

---
+++ gmail-clone/src/components/organisms/MailTable.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Button from "../atoms/Button.vue";
+export default defineComponent({
+  components: {
+    Button
+  },
+  props: {
+    emails: { type: Array as PropType<Array<Email>> },
+  },
+    setup() {
+
+    const handleCreate = () => {
+      console.log('Child has been created.............');
+    }
+
+
+    return { handleCreate}
+  },
+})
+</script>
+
+<template>
+  <table class="mail-table">
+    <tbody>
+      <tr v-for="email in emails" :key="email.id">
+        <td>
+          <input type="checkbox" />
+        </td>
+        <td>{{ email.from }}</td>
+        <td>
+          <p>
+            <strong>{{ email.subject }}</strong>
+            - {{ email.body }}
+          </p>
+        </td>
+        <td class="date">{{ new Date(email.sentAt) }}</td>
+        <td>
+          <Button
+            @click="handleCreate"
+            content="Archive"
+            classes="bg-green-500"
+          ></Button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
