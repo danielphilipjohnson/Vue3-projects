fee7dd9a9b3aa9bc4dcf9be03124313229ff11e9
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-01-28 22:18:56 +0000

    Made email filtering composable

---
+++ gmail-clone/src/composables/useEmailFiltering.ts
@@ -0,0 +1,33 @@
+import { ref, Ref, computed } from "vue";
+import { Email } from "../types/email";
+
+export const useEmailSelection = (
+  emails: Ref<Array<Email>>,
+  screenMode: Ref<string>
+) => {
+  const sortedEmails = computed(() => {
+    return emails.value.sort((email1, email2) => {
+      return email1.sentAt < email2.sentAt ? 1 : -1;
+    });
+  });
+
+  const unarchivedEmails = computed(() => {
+    return sortedEmails.value.filter((e) => !e.archived);
+  });
+
+  const archivedEmails = computed(() => {
+    return sortedEmails.value.filter((e) => e.archived);
+  });
+
+  const filteredEmails = computed(() => {
+    const filters: Ref = ref({
+      inbox: unarchivedEmails,
+      archive: archivedEmails,
+    });
+    return filters.value[screenMode.value];
+  });
+
+  return {
+    filteredEmails,
+  };
+};
