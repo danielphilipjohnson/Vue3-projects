52b88e36a8baf80f4c751c1e972a3eff33fe285a
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-01-28 21:18:24 +0000

    built bulk action bar

---
+++ gmail-clone/src/components/organisms/BulkActionBar.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from "../atoms/Button.vue";
+
+export default defineComponent({
+  components: {
+    Button
+  },
+
+  setup() {
+    const handleCreate = () => {
+      console.log('Child has been created.............');
+    }
+
+    return { handleCreate }
+  },
+
+})
+</script>
+
+
+<template>
+	<div class="bulk-action-bar">
+      <span class="checkbox">
+        <input type="checkbox" :class="[partialSelection ? 'partial-check' : '']" />
+      </span>
+
+      <span class="buttons">
+        <Button
+          @click="handleCreate"
+          content="Mark Read"
+          classes="bg-blue-500"
+        ></Button>
+
+        <Button
+          @click="handleCreate"
+          content="Mark Unread"
+          classes="bg-blue-500"
+        ></Button>
+        <Button
+          v-if="selectedScreen == 'inbox'"
+          @click="handleCreate"
+          content="Archive"
+          classes="bg-blue-500"
+        ></Button>
+        <Button
+          v-else
+          @click="handleCreate"
+          content="Move to Inbox"
+
+          classes="bg-blue-500"
+        ></Button>
+      </span>
+    </div>
+</template>
