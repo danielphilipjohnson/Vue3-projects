13d28c29e1bc8236a771ac2b5d18b5a3f6c0c2ab
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-01-29 13:07:58 +0000

    created modal template

---
+++ gmail-clone/src/components/organisms/Modal.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+export default defineComponent({
+  setup() {},
+  props: {
+    closeModal: { type: Function as PropType<Function>, required: true},
+  },
+});
+</script>
+
+<template>
+  <div class="modal">
+    <div class="overlay" @click="closeModal"></div>
+    <div class="modal-card">
+		test
+      <slot />
+    </div>
+  </div>
+</template>
