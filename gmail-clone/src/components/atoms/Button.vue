9caf0a0db5b37515e84ae66f9534f416f7e3db62
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-01-29 14:33:52 +0000

    Updated the buttom atom

--- gmail-clone/src/components/atoms/Button.vue
+++ gmail-clone/src/components/atoms/Button.vue
@@ -4,7 +4,7 @@ import { defineComponent, PropType } from 'vue'
 export default defineComponent({
   props: {
     created: {type:Function as PropType<Function>},
-    content: {type:String as PropType<string>, required: true},
+    content: {type:String as PropType<string>},
 	classes: {type:String as PropType<string>, required: true},
   }
 })
@@ -13,8 +13,10 @@ export default defineComponent({
 <template>
 	<button
 	  type="button"
-		@click="this.$emit('buttonClick')"
+		@click="$emit('buttonClick')"
 		class="border border-gray-200"
 		:class="classes"
-	>{{content}}</button>
+	>{{content}}
+	<slot/>
+	</button>
 </template>
