b779ef2dacb3a965a079de56c9f5de911eb2f2b7
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-01-29 12:54:41 +0000

    Refactor the view

--- gmail-clone/src/views/HomeView.vue
+++ gmail-clone/src/views/HomeView.vue
@@ -1,91 +1,97 @@
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue'
+import { defineComponent, ref, computed } from "vue";
 import Button from "../components/atoms/Button.vue";
 import MailTable from "../components/organisms/MailTable.vue";
 import BulkActionBar from "../components/organisms/BulkActionBar.vue";
-import { useEmailSelection } from '../composables/useEmailFiltering';
+import { useEmailFiltering } from "../composables/useEmailFiltering";
+import { useEmailSelection } from "../composables/useEmailSelection";
+
 export default defineComponent({
   components: {
-    BulkActionBar, Button, MailTable
+    BulkActionBar,
+    Button,
+    MailTable,
   },
 
   setup() {
-
     const emails = ref([
       {
-        "id": 1,
-        "from": "team@vuemastery.com",
-        "subject": "What's up with Vue 3.0? Here's how to find out from Evan You",
-        "body": "The opening keynote of VueConf US this year was Evan You (the creator of Vue), giving his State of the Vuenion address. He walked us through the journey of getting Vue 3 from a prototype to a reality the past year. He also dove into Vue's overall growth in the community.",
-        "sentAt": "2020-03-27T18:25:43.511Z",
-        "archived": false,
-        "read": false
+        id: 1,
+        from: "team@vuemastery.com",
+        subject: "What's up with Vue 3.0? Here's how to find out from Evan You",
+        body: "The opening keynote of VueConf US this year was Evan You (the creator of Vue), giving his State of the Vuenion address. He walked us through the journey of getting Vue 3 from a prototype to a reality the past year. He also dove into Vue's overall growth in the community.",
+        sentAt: "2020-03-27T18:25:43.511Z",
+        archived: false,
+        read: false,
       },
       {
-        "id": 2,
-        "from": "jeffrey@vuetraining.net",
-        "subject": "Learn by doing - Vue 3 Zero to Intermediate in 8 weeks",
-        "body": "Building projects is one of the most effective ways to learn - and _the_ most effective way _remember_ what you've learned - but it can be frustrating.\n\nThis 8-week course takes the pain out of 'learning by doing'.\n\nEach week we give you\n\n* a project that will grow your skills without overwhelming you\n* links to hand-picked resources, such as Vue Mastery videos, that share the knowledge you'll need for the project (no more useless rabbit holes)\n* answers to any and all questions you have while working\n* feedback on your completed code (so you're only learning good habits)\n\nOur instructors are standing by to answer your questions.\n\nReady to learn?",
-        "sentAt": "2020-05-20T18:25:43.511Z",
-        "archived": false,
-        "read": false
+        id: 2,
+        from: "jeffrey@vuetraining.net",
+        subject: "Learn by doing - Vue 3 Zero to Intermediate in 8 weeks",
+        body: "Building projects is one of the most effective ways to learn - and _the_ most effective way _remember_ what you've learned - but it can be frustrating.\n\nThis 8-week course takes the pain out of 'learning by doing'.\n\nEach week we give you\n\n* a project that will grow your skills without overwhelming you\n* links to hand-picked resources, such as Vue Mastery videos, that share the knowledge you'll need for the project (no more useless rabbit holes)\n* answers to any and all questions you have while working\n* feedback on your completed code (so you're only learning good habits)\n\nOur instructors are standing by to answer your questions.\n\nReady to learn?",
+        sentAt: "2020-05-20T18:25:43.511Z",
+        archived: false,
+        read: false,
       },
       {
-        "id": 3,
-        "from": "damian@dulisz.com",
-        "subject": "#177: Updated Vue.js Roadmap; Vuex v4.0.0-alpha.1 has been released; Kia King Ishii join the core team; Nuxt v2.12 released; Videos from Vue.js Amsterdam 2020 are here!",
-        "body": "First of all, lets congratulate Kia King Ishii on joining the Vue.js core team! 🎉 He has been doing an incredible job building vuex-orm and will now focus on working on the next versions of Vuex.\n\nSpeaking of which – Vuex v4.0.0-alpha.1 has just been released! This is the version of Vuex that will work with Vue 3.0 but keep the familiar API you know from the current version.",
-        "sentAt": "2020-03-18T18:25:43.511Z",
-        "archived": false,
-        "read": true
+        id: 3,
+        from: "damian@dulisz.com",
+        subject:
+          "#177: Updated Vue.js Roadmap; Vuex v4.0.0-alpha.1 has been released; Kia King Ishii join the core team; Nuxt v2.12 released; Videos from Vue.js Amsterdam 2020 are here!",
+        body: "First of all, lets congratulate Kia King Ishii on joining the Vue.js core team! 🎉 He has been doing an incredible job building vuex-orm and will now focus on working on the next versions of Vuex.\n\nSpeaking of which – Vuex v4.0.0-alpha.1 has just been released! This is the version of Vuex that will work with Vue 3.0 but keep the familiar API you know from the current version.",
+        sentAt: "2020-03-18T18:25:43.511Z",
+        archived: false,
+        read: true,
       },
       {
-        "id": 4,
-        "from": "anthony@vuejsdevelopers.com",
-        "subject": "'Vue 3 Release Roadmap' + 6 more must-read articles from this week",
-        "body": "Newsletter Issue #161",
-        "sentAt": "2020-03-24T18:25:43.511Z",
-        "archived": true,
-        "read": true
-      }
+        id: 4,
+        from: "anthony@vuejsdevelopers.com",
+        subject:
+          "'Vue 3 Release Roadmap' + 6 more must-read articles from this week",
+        body: "Newsletter Issue #161",
+        sentAt: "2020-03-24T18:25:43.511Z",
+        archived: true,
+        read: true,
+      },
     ]);
+    
     const selectedScreen = ref("inbox");
+     const selectScreen = (newScreen) => {
+      selectedScreen.value = newScreen;
+      emailSelection.clear();
+    };
 
-    const {filteredEmails} = useEmailSelection(emails, selectedScreen);
- 
+    const { filteredEmails } = useEmailFiltering(emails, selectedScreen);
+    const emailSelection = useEmailSelection();
 
-    const selectScreen = (newScreen) => {
-        selectedScreen.value = newScreen;
-    }
-
-
-
-
-    const handleCreate = () => {
-      console.log('Child has been created.............');
-    }
     const partialSelection = ref("partial-check");
-    return { handleCreate, selectedScreen, selectScreen, partialSelection, filteredEmails }
+
+    return {
+
+      selectedScreen,
+      selectScreen,
+      partialSelection,
+      filteredEmails,
+      emailSelection: useEmailSelection()
+    };
   },
-
-})
-
+});
 </script>
 
 <template>
   <main>
     <Button
-      @click="selectScreen('inbox');"
+      @click="selectScreen('inbox')"
       content="Inbox View"
       :class="[selectedScreen == 'inbox' ? 'selected' : '']"
     ></Button>
     <Button
-      @click="selectScreen('archive');"
+      @click="selectScreen('archive')"
       content="Archived View"
       :class="[selectedScreen == 'archive' ? 'selected' : '']"
       classes="bg-blue-500"
     ></Button>
-    <h1>GMail clone</h1>
+    <h1>GMail clone: {{selectedScreen}}</h1>
 
     <BulkActionBar />
     <MailTable :emails="filteredEmails" />
