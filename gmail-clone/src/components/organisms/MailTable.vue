b979c38d6ceefa30fc1a7420db4923fe0400318e
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-01-29 14:52:23 +0000

    built logic for change email

--- gmail-clone/src/components/organisms/MailTable.vue
+++ gmail-clone/src/components/organisms/MailTable.vue
@@ -11,13 +11,12 @@ export default defineComponent({
   components: {
     Button,
     Modal,
-    MailView
+    MailView,
   },
   props: {
-    emails: { type: Array as PropType<Array<Email>> },
+    emails: { type: Array as PropType<Array<Email>>, required: true},
   },
-  setup() {
-
+  setup({emails}) {
     const openedEmail = ref();
 
     // Sets open email
@@ -31,13 +30,45 @@ export default defineComponent({
 
     const archiveEmail = (email: Email) => {
       email.archived = true;
+    };
 
-    }
+    const changeEmail = (
+      email: Email,
+      {
+        indexChange,
+        toggleArchive,
+        toggleRead,
+        save,
+        closeModal,
+      }: { indexChange: number; toggleArchive: Boolean, toggleRead: Boolean, save: Boolean, closeModal: Boolean}
+    ) => {
+      if (toggleArchive) {
+        email.archived = !email.archived;
+      }
+      if (toggleRead) {
+        email.read = !email.read;
+      }
+      if(save) { console.log("save later") }
+      if (closeModal) {
+        openedEmail.value = null;
+        return null;
+      }
 
-    return { openEmail, openedEmail, emailSelection: useEmailSelection(), archiveEmail };
+      if (indexChange) {
+        let index = emails.findIndex((e) => e == email);
+        openEmail(emails[index + indexChange]);
+      }
+    };
+
+    return {
+      openEmail,
+      openedEmail,
+      emailSelection: useEmailSelection(),
+      archiveEmail,
+      changeEmail
+    };
   },
 });
-
 </script>
 
 <template>
@@ -60,13 +91,24 @@ export default defineComponent({
         </td>
         <td class="date">{{ new Date(email.sentAt) }}</td>
         <td>
-          <Button @click="archiveEmail(email)" content="Archive" classes="bg-green-500"></Button>
+          <Button
+            @click="archiveEmail(email)"
+            content="Archive"
+            classes="bg-green-500"
+          ></Button>
         </td>
       </tr>
     </tbody>
   </table>
 
-  <Modal v-if="openedEmail" :closeModal="() => { openedEmail = null; }">
-    <MailView :email="openedEmail" />
+  <Modal
+    v-if="openedEmail"
+    :closeModal="
+      () => {
+        openedEmail = null;
+      }
+    "
+  >
+    <MailView :email="openedEmail" :changeEmail="(args) => changeEmail(openedEmail, args)" />
   </Modal>
 </template>
