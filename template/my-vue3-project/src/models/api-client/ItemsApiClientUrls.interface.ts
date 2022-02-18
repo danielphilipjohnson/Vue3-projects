0ea6bd8cce67b0783da526690fedad26f2f393d5
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-02-18 21:46:46 +0000

    Chore Create abstraction for API requests

---
+++ template/my-vue3-project/src/models/api-client/ItemsApiClientUrls.interface.ts
@@ -0,0 +1,10 @@
+import ItemInterface  from '@/models/items/item.interface'
+
+/**
+ * @Name ItemsApiClientInterface
+ * @description
+ * Interface for the Items api client module
+ */
+export interface ItemsApiClientInterface {
+  fetchItems: () => Promise<ItemInterface[]>
+}
