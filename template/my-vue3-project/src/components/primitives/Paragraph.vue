96bc0b46cd29d85a551ef753fc4eb92bf8766e2d
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-02-21 21:06:25 +0000

    Added basic paragraph

---
+++ template/my-vue3-project/src/components/primitives/Paragraph.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="pb-2 leading-7">
+    {{ content }}
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+export default defineComponent({
+  props: {
+    content: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+})
+</script>
