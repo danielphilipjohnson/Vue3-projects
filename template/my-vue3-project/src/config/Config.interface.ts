6d4a74f3a901e4dc07cc002587907d65a60383f5
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-02-21 20:12:10 +0000

    Created config files

---
+++ template/my-vue3-project/src/config/Config.interface.ts
@@ -0,0 +1,30 @@
+import { ItemsApiClientUrlsInterface } from "@/models/api-client/items";
+import { LocaleInfoInterface } from "@/models/localization/LocaleInfo.interface";
+import { ThemeInfoInterface } from "my-component-library";
+
+/**
+ * @Name ConfigInterface
+ * @description
+ *
+ */
+export interface ConfigInterface {
+  global: {
+    // ... things that are not specific to a single app domain
+  };
+
+  locales: LocaleInfoInterface[];
+
+  themes: ThemeInfoInterface[];
+
+  httpClient: {
+    tokenKey: string;
+  };
+
+  apiClient: {
+    type: string;
+  };
+
+  items: {
+    apiUrls: ItemsApiClientUrlsInterface;
+  };
+}
