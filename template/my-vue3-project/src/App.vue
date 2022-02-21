14b2f5e668a3417df25a3301712e7707a866b24b
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-02-21 20:16:28 +0000

    Updated tailwind Path

--- template/my-vue3-project/src/App.vue
+++ template/my-vue3-project/src/App.vue
@@ -1,41 +1,19 @@
 <template>
   <div id="app">
-    <h2>My Vue 3 Project</h2>
+    <div class="long-date">{{ new Date() }}</div>
+    <h2>welcome</h2>
     <div id="nav" class="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
     </div>
     <router-view />
   </div>
 </template>
+
 <script lang="ts">
-  import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
-  export default defineComponent({
-    name: 'App'
-  })
+export default defineComponent({
+  name: "App",
+});
 </script>
-
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- template/my-vue3-project/src/main.ts
+++ template/my-vue3-project/src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { rootStore } from "./store";
-import "./assets/scss/tailwind/style.css";
+import "./assets/tailwind.css";
 
 createApp(App).use(rootStore).use(router).mount("#app");
