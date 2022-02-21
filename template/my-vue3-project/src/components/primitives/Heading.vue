f1053a8954b3ad49d75f4bbcef0c20feacfbf7e0
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-02-21 21:01:02 +0000

    Added base heading

---
+++ template/my-vue3-project/src/components/primitives/Heading.vue
@@ -0,0 +1,36 @@
+<template>
+  <component :is="tag" :class="classes[tag]">
+    {{ content }}
+  </component>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    tag: {
+      type: String as PropType<string>,
+      required: true,
+      validator: (value) =>
+        ["h1", "h2", "h3", "h4", "h5", "h6"].includes(value.toLowerCase()),
+    },
+    content: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      classes: {
+        h1: "text-3xl md:text-4xl font-bold pb-2 mb-3 border-b border-grey",
+        h2: "text-3xl md:text-4xl mr-8 uppercase text-sm font-bold text-light-gray mb-2",
+        h3: "text-2xl md:text-3xl mr-8 uppercase text-sm font-bold text-light-gray mb-2",
+        h4: "text-xl md:text-2xl mr-8 uppercase text-sm font-bold text-light-gray mb-2",
+        h5: "mr-8 uppercase text-sm font-bold text-light-gray mb-2",
+        h6: "mr-8 uppercase text-sm font-bold text-light-gray mb-2",
+      },
+    };
+  },
+});
+</script>
