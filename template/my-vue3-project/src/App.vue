a5cc7fc39340eeeaf467caf8843fe1e97ceb5f21
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-02-21 21:23:03 +0000

    Added footer and nav to app

--- template/my-vue3-project/src/App.vue
+++ template/my-vue3-project/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <TheNav />
     <div class="long-date">{{ new Date() }}</div>
     <h2>welcome</h2>
     <div id="nav" class="nav">
@@ -7,13 +8,19 @@
       <router-link to="/about">About</router-link> |
     </div>
     <router-view />
+    <TheFooter />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import TheNav from "./TheNav.vue";
+import TheFooter  from "./TheFooter.vue";
 export default defineComponent({
   name: "App",
+  components: {
+    TheFooter,
+    TheNav
+  },
 });
 </script>
