91857b7ecd46900e7fa9ab27333f040b9bc049eb
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-04 21:42:27 +0000

    Added logic to display the user block

--- vue-chat/src/views/HomeView.vue
+++ vue-chat/src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <TheLogin />
+    <UserBlock v-slot="{ user }">
+      <UserProfile v-if="user" :user="user" />
+      <TheLogin v-else />
+    </UserBlock>
   </div>
 </template>
 
 <script lang="ts">
 import TheLogin from "../components/TheLogin.vue";
+import UserBlock from "../components/UserBlock.vue";
+import UserProfile from "../components/UserProfile.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
     TheLogin,
+    UserBlock,
+    UserProfile,
   },
 });
 </script>
