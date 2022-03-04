1501eacf026013c967302667ea8c4ec9ddc9c995
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-04 20:31:44 +0000

    Added login  to home

--- vue-chat/src/views/HomeView.vue
+++ vue-chat/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Home</h1>
+    <TheLogin />
   </div>
 </template>
 
 <script lang="ts">
+import TheLogin from "../components/TheLogin.vue";
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    TheLogin,
   },
 });
 </script>
