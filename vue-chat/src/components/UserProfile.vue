4552abb9d50b7563d170c2cc9eb074e187a85ded
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-04 21:42:03 +0000

    Added basic user profile

---
+++ vue-chat/src/components/UserProfile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Logged in as {{ user.uid }}
+    <br />
+    <button @click="auth.signOut()" class="button">Sign Out</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "@vue/runtime-core";
+import { auth } from "../firebase";
+
+export default defineComponent({
+  setup() {
+    return {
+      auth,
+    };
+  },
+  props: {
+    user: { type: Object as PropType<Record<string, unknown>>, required: true },
+  },
+});
+</script>
