f94d10fe32e8050321d03543f07b750320f990df
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-04 20:32:19 +0000

    Created login component

---
+++ vue-chat/src/components/TheLogin.vue
@@ -0,0 +1,18 @@
+<template>
+  <aside>
+    <h3>Sign in Anonymously</h3>
+    <button type="button" @click="auth.signInAnonymously()">Sign In</button>
+  </aside>
+</template>
+<script lang="ts">
+import { auth } from "../firebase";
+
+export default {
+  name: "TheLogin",
+  setup() {
+    return {
+      auth,
+    };
+  },
+};
+</script>
