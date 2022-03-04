d9361f53aec50420ad7b9b7cadc31d43960f3d07
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-04 20:07:20 +0000

    Aded firebase config

--- vue-chat/src/firebase.ts
+++ vue-chat/src/firebase.ts
@@ -1,10 +1,16 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
-const firebaseConfig = { 
-    //TODO
+const firebaseConfig = {
+  apiKey: "AIzaSyCYxfvw2vCUzHNeAp9Wy_yG9uJa8BSA_oc",
+  authDomain: "chatapp-fff12.firebaseapp.com",
+  projectId: "chatapp-fff12",
+  storageBucket: "chatapp-fff12.appspot.com",
+  messagingSenderId: "754405013773",
+  appId: "1:754405013773:web:cf654fc57438d178ef1844",
+  measurementId: "G-QSSSMK5EGK",
 };
 
 firebase.initializeApp(firebaseConfig);
