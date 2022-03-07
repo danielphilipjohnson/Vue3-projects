090acef01140bd4ea4a2277d69714f41a4d5a6e1
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-07 19:19:14 +0000

    reduced giphy images to a container

---
+++ vue-chat/src/components/GiphyCards.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <template v-for="gif in gifs" :key="gif.id">
+      <img :src="gif" alt="" @click="$emit('someEvent', $event)" />
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    gifs: { type: Array as PropType<Array<string>>, required: true },
+  },
+});
+</script>
