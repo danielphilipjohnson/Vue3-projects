843c20b9000f008a56c6565e4925529cc36c7bfb
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-07 21:16:36 +0000

    Extracted emoji container

---
+++ vue-chat/src/components/EmojiContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <template>
+    <div v-show="displayEmoji">
+      <VuemojiPicker @emojiClick="$emit('handleEmojiClick', $event)" />
+    </div>
+    <button @click="showEmoji()">:D</button>
+  </template>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+import { VuemojiPicker } from "vuemoji-picker";
+
+export default defineComponent({
+  components: {
+    VuemojiPicker,
+  },
+  setup() {
+    const displayEmoji = ref(false);
+
+    const showEmoji = () => {
+      displayEmoji.value = !displayEmoji.value;
+    };
+
+    return {
+      showEmoji,
+      displayEmoji,
+    };
+  },
+});
+</script>
