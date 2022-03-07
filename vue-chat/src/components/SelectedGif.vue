fcb26e4006d1a69521bcfeff6096cf45923f1067
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-07 19:19:29 +0000

    seperated selected gif to its own container

---
+++ vue-chat/src/components/SelectedGif.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <img :src="selectedGif" alt="placeholder" />
+    <input v-model="newMessageText" class="input" type="text" />
+    <button @click="sendGif(selectedGif)">Submit</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from "vue";
+import { getAuth } from "firebase/auth";
+import { createMessage, getMessageCollection } from "../firestore-client/";
+
+export default defineComponent({
+  props: {
+    selectedGif: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    chatID: { type: String as PropType<string>, required: true },
+  },
+  setup(chatID) {
+    const auth = getAuth();
+    const newMessageText = ref("");
+
+    const newMessageRef = getMessageCollection(chatID.chatID);
+
+    const sendGif = (selectedGif: string) => {
+      if (auth.currentUser?.uid) {
+        createMessage(
+          newMessageRef,
+          newMessageText,
+          auth.currentUser?.uid,
+          null,
+          selectedGif
+        );
+      } else {
+        throw new Error("Couldnt send your message. Please try again.");
+      }
+    };
+    return {
+      sendGif,
+      newMessageText,
+    };
+  },
+});
+</script>
