ec76472a2fed0f58ae3b328abbbdb82aba6d574d
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-09 20:12:23 +0000

    added colors

--- vue-chat/tailwind.config.js
+++ vue-chat/tailwind.config.js
@@ -1,3 +1,6 @@
+const colors = require('tailwindcss/colors')
+
+
 module.exports = {
   content: [
     "./index.html",
@@ -5,6 +8,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      'woodsmoke': { DEFAULT: '#161819', '50': '#6C767B', '100': '#626B70', '200': '#4F575A', '300': '#3C4244', '400': '#292D2F', '500': '#161819', '600': '#000000', '700': '#000000', '800': '#000000', '900': '#000000' },
+    }
   },
   plugins: [],
 }
